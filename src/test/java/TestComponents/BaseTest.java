59fdeaf6debdeabd06e6017d9a116cbac428807a
ArunDrioder <arunprasadh.s@gmail.com>
2024-06-10 23:52:13 +0530

    New Things were added in the Frameworks

--- src/test/java/TestComponents/BaseTest.java
+++ src/test/java/TestComponents/BaseTest.java
@@ -5,12 +5,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.commons.io.FileUtils;
 import org.example.Pages.LandingPage;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -34,13 +37,21 @@ public class BaseTest
         prop.load(fis);
         String browserName = System.getProperty("browser")!=null ? System.getProperty("browser") : prop.getProperty("browser");
          //prop.getProperty("browser");
-        if (browserName.equalsIgnoreCase("chrome"))
+        if (browserName.contains("chrome"))
         {
+            ChromeOptions options = new ChromeOptions();
             WebDriverManager.chromedriver().setup();
-             driver = new ChromeDriver();
+           if (browserName.contains("headless"))
+           {
+               options.addArguments("headless");
+           }
+           driver = new ChromeDriver(options);
+           driver.manage().window().setSize(new Dimension(1920,1080));
 
         } else if (browserName.equalsIgnoreCase("firefox"))
         {
+            FirefoxOptions options = new FirefoxOptions();
+            options.addArguments("headless");
             WebDriverManager.firefoxdriver().setup();
              driver = new FirefoxDriver();
 
